8eb7d5ba229416f456455ad0f1314fee053b5d23
muxueqz <zhangmingyuan240@gmail.com>
2021-12-22 10:49:57 +0800

    bugfix. fix  ${PLUGIN_BRANCH}

--- script.sh
+++ script.sh
@@ -5,13 +5,15 @@ echo "${PLUGIN_SSH_KEY}" > $HOME/.ssh/id_rsa
 chmod 400 $HOME/.ssh/id_rsa
 cd ${PLUGIN_PATH}
 
+[ -z $PLUGIN_BRANCH: ] && PLUGIN_BRANCH='master'
+
 export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no'
 git config --global user.name $DRONE_COMMIT_AUTHOR
 git config --global user.email $DRONE_COMMIT_AUTHOR_EMAIL
-git init
+git init -b ${PLUGIN_BRANCH}
 git add .
 [ -z $PLUGIN_COMMIT_MESSAGE ] && PLUGIN_COMMIT_MESSAGE='push'
-[ -z $PLUGIN_BRANCH: ] && PLUGIN_BRANCH='master'
+
 [ $PLUGIN_FORCE == "true" ] && PLUGIN_FORCE='--force'
 git commit . -m "${PLUGIN_COMMIT_MESSAGE}"
 git remote add origin ${PLUGIN_REMOTE}
