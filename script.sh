0334ff743110002e723ceb7c59fd9e8ebcf2d6de
muxueqz <zhangmingyuan240@gmail.com>
2021-12-22 11:22:14 +0800

    feat: support not force mode

--- script.sh
+++ script.sh
@@ -5,16 +5,25 @@ echo "${PLUGIN_SSH_KEY}" > $HOME/.ssh/id_rsa
 chmod 400 $HOME/.ssh/id_rsa
 cd ${PLUGIN_PATH}
 
-[ -z $PLUGIN_BRANCH: ] && PLUGIN_BRANCH='master'
-
 export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no'
+
 git config --global user.name $DRONE_COMMIT_AUTHOR
 git config --global user.email $DRONE_COMMIT_AUTHOR_EMAIL
-git init -b ${PLUGIN_BRANCH}
-git add .
-[ -z $PLUGIN_COMMIT_MESSAGE ] && PLUGIN_COMMIT_MESSAGE='push'
 
+[ -z $PLUGIN_BRANCH: ] && PLUGIN_BRANCH='master'
+[ -z $PLUGIN_COMMIT_MESSAGE ] && PLUGIN_COMMIT_MESSAGE=${DRONE_COMMIT_MESSAGE}
 [ $PLUGIN_FORCE == "true" ] && PLUGIN_FORCE='--force'
+
+if [ $PLUGIN_INIT == "true" ];then
+  git init -b ${PLUGIN_BRANCH}
+  git remote add origin ${PLUGIN_REMOTE}
+fi
+
+if [ ! $PLUGIN_INIT == "true" ];then
+  git clone ${PLUGIN_REMOTE} -b ${PLUGIN_BRANCH} ../tmp_for_push
+  cp -av . ../tmp_for_push/
+  cd ../tmp_for_push
+fi
+git add .
 git commit . -m "${PLUGIN_COMMIT_MESSAGE}"
-git remote add origin ${PLUGIN_REMOTE}
 git push --set-upstream origin ${PLUGIN_BRANCH} ${PLUGIN_FORCE}
