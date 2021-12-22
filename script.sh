3773c73d6edae38c5dfbb63b6571942759d0f033
muxueqz <zhangmingyuan240@gmail.com>
2021-12-22 11:36:06 +0800

    bugfix. fix if

--- script.sh
+++ script.sh
@@ -10,16 +10,16 @@ export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no'
 git config --global user.name $DRONE_COMMIT_AUTHOR
 git config --global user.email $DRONE_COMMIT_AUTHOR_EMAIL
 
-[ -z $PLUGIN_BRANCH: ] && PLUGIN_BRANCH='master'
+[ -z $PLUGIN_BRANCH ] && PLUGIN_BRANCH='master'
 [ -z $PLUGIN_COMMIT_MESSAGE ] && PLUGIN_COMMIT_MESSAGE=${DRONE_COMMIT_MESSAGE}
-[ $PLUGIN_FORCE == "true" ] && PLUGIN_FORCE='--force'
+[ "$PLUGIN_FORCE" == "true" ] && PLUGIN_FORCE='--force'
 
-if [ $PLUGIN_INIT == "true" ];then
+if [ "$PLUGIN_INIT" == "true" ];then
   git init -b ${PLUGIN_BRANCH}
   git remote add origin ${PLUGIN_REMOTE}
 fi
 
-if [ ! $PLUGIN_INIT == "true" ];then
+if [ ! "$PLUGIN_INIT" == "true" ];then
   git clone ${PLUGIN_REMOTE} -b ${PLUGIN_BRANCH} ../tmp_for_push
   cp -av . ../tmp_for_push/
   cd ../tmp_for_push
